f0a423bb333b41bc663901efc78cb3110eb6b8ba
SobolevvMaxim <brtgfc@bk.ru>
2021-07-20 18:58:29 +0300

    test pull

--- src/com/company/Run.java
+++ src/com/company/Run.java
@@ -10,5 +10,7 @@ public class Run {
     public static void main(String[] args) throws IOException {
         //TODO run method
         MainMenu.mainMenu();
+
+        System.out.println("Test pull");
     }
 }
