941600d2c9aceb2c1ccd92e81283378fa108ae55
Admin <brtgfc@bk.ru>
2021-07-27 20:05:43 +0300

    test

---
+++ src/com/company/menu/ResultsMenu.java
@@ -0,0 +1,13 @@
+package com.company.menu;
+
+public class ResultsMenu {
+
+    public static void resultsMenu(){
+        System.out.println("1 - вывести в порядке убывания\n" +
+                "2 - найти пользователя");
+
+        int choice;
+
+        // TODO Result menu
+    }
+}
